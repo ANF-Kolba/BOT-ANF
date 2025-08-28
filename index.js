96170c3d5a66660e9ef7e849fb870fcdcae76f3e
ANF-Kolba <christoferkolba14@gmail.com>
2025-08-28 16:45:39 -0300

    Bot inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.on('ready', () => {
+  console.log(`🤖 Bot conectado como ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('🏓 Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);

496e8ba389619f041042d0db1857fdb77fc35d0f
ANF-Kolba <christoferkolba14@gmail.com>
2025-08-29 19:22:39 -0300

    hug update

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -1,24 +1,26 @@
 import { EmbedBuilder } from 'discord.js';
-console.log(`Comando hug chamado por ${message.author.tag}`);
+
 export default {
   name: "hug",
   description: "Abraça alguém!",
   async execute(message) {
+    console.log(`Comando hug chamado por ${message.author.tag}`);
+
     const user = message.mentions.users.first();
     if (!user) {
       return message.reply("❌ Mencione alguém para abraçar!");
     }
 
     const gifs = [
-  "https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif",
-  "https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif",
-  "https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif",
-  "https://media.giphy.com/media/3ZnBrkqoaI2hq/giphy.gif",
-  "https://media.giphy.com/media/wnsgren9NtITS/giphy.gif",
-  "https://media.giphy.com/media/duzpaTbCUy9Vu/giphy.gif",
-  "https://media.giphy.com/media/svXXBgduBsJ1u/giphy.gif",
-  "https://media.giphy.com/media/49mdjsMrH7oze/giphy.gif"
-];
+      "https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif",
+      "https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif",
+      "https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif",
+      "https://media.giphy.com/media/3ZnBrkqoaI2hq/giphy.gif",
+      "https://media.giphy.com/media/wnsgren9NtITS/giphy.gif",
+      "https://media.giphy.com/media/duzpaTbCUy9Vu/giphy.gif",
+      "https://media.giphy.com/media/svXXBgduBsJ1u/giphy.gif",
+      "https://media.giphy.com/media/49mdjsMrH7oze/giphy.gif"
+    ];
 
     const gif = gifs[Math.floor(Math.random() * gifs.length)];
 

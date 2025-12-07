8b4c67fe7d49b32a7e446c7850566e2bdcb48d85
ANF-Kolba <christoferkolba14@gmail.com>
2025-12-07 09:10:08 -0300

    db

--- utils/database.js
+++ utils/database.js
@@ -3,12 +3,15 @@ import { Sequelize, DataTypes } from "sequelize";
 // ---------------------- CONEXÃO ----------------------
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  protocol: "postgres",
   logging: false,
   dialectOptions: {
-    ssl: { require: true, rejectUnauthorized: false },
-  },
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
+
 
 // ---------------------- MODELOS ----------------------
 

e03cb149ac98c3158ac82fcfb00b45485d00864f
ANF-Kolba <christoferkolba14@gmail.com>
2025-12-07 09:55:08 -0300

    db

--- utils/database.js
+++ utils/database.js
@@ -8,6 +8,12 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectModule: pg,
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 // ---------------------- MODELOS ----------------------
 
